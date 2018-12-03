5d4ed11d74208630aad3658ba76594e72cc48abd
anandkelkar <kelkaranand28@gmail.com>
2018-12-03 11:13:21 -0500

    Initial Commit

---
+++ CheckIn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckIn
+//
+//  Created by Anand Kelkar on 03/12/18.
+//  Copyright © 2018 Anand Kelkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

95a0760b9d174ee8c737111590c6242621680df1
Asteezy24 <alxtv1026@gmail.com>
2018-12-12 14:44:03 -0500

    Event Details page, Create Event page as well as transitions

--- CheckIn/Admin Tools/AdminViewController.swift
+++ CheckIn/Admin Tools/AdminViewController.swift
@@ -7,9 +7,41 @@
 //
 
 import UIKit
-class ehh: UIViewController {
+class AdminViewController: UIViewController {
+    
+    @IBOutlet var uploadDataView: UIView!
+    @IBOutlet var downloadDataView: UIView!
+    @IBOutlet var filterView: UIView!
+    @IBOutlet var createEventView: UIView!
+    @IBOutlet var eventDetailsView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.title = "Alex"
+        setupGestureRecognizers()
     }
+    
+    fileprivate func setupGestureRecognizers() {
+        uploadDataView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        downloadDataView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        filterView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        createEventView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(openCreateEventVC)))
+        eventDetailsView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(openEventDetailsVC)))
+    }
+    
+    @objc func handleTap(){
+        print("TOUCHED")
+    }
+    
+    @objc func openCreateEventVC() {
+        let storyboard: UIStoryboard = UIStoryboard(name: "CreateEvent", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "CreateEvent") as! CreateEventViewController
+        self.show(vc, sender: self)
+    }
+    
+    @objc func openEventDetailsVC() {
+        let storyboard: UIStoryboard = UIStoryboard(name: "EventDetails", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "EventDetails") as! EventDetailsViewController
+        self.show(vc, sender: self)
+    }
+    
 }
---
+++ CheckIn/Create Event/CreateEventViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreateEventViewController.swift
+//  CheckIn
+//
+//  Created by Alexander Stevens on 12/12/18.
+//  Copyright © 2018 Anand Kelkar. All rights reserved.
+//
+
+import UIKit
+
+class CreateEventViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

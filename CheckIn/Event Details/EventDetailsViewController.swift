9cd611219431134ab94649dbd4c1005ae30e5758
Asteezy24 <alxtv1026@gmail.com>
2018-12-12 14:53:39 -0500

    missed some view controllers

---
+++ CheckIn/Event Details/EventDetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EventDetails.swift
+//  CheckIn
+//
+//  Created by Alexander Stevens on 12/12/18.
+//  Copyright © 2018 Anand Kelkar. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
